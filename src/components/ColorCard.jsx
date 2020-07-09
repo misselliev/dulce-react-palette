301c06527a0993a1e96dcd928416907eed40fb1d
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-07-09 14:17:16 -0700

    Updating ColorCard component and adding propTypes

--- src/components/ColorCard.jsx
+++ src/components/ColorCard.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'proptypes';
+import ColorDrop from './ColorDrop';
 
-
-function ColorCard({colorArray}) {
-  return (
-    <div className="ui raised card">
-      colorArray.map(color => (
-        <ColorDrop key={color} color={color} />
-      ))
-    </div>
-  )
-}
+const ColorCard = ({ colorArray }) => (
+  <div className="ui raised card">
+    {colorArray.map(color => (
+      <ColorDrop key={color} color={color} />
+    ))}
+  </div>
+);
 
 ColorCard.propTypes = {
   colorArray: PropTypes.instanceOf(Array).isRequired,
 };
-
 
 export default ColorCard;
