abcdabbec1b5428a7301db42c297cd076fa613fe
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-07-09 20:21:32 -0700

    Updating Menubar with icons

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -3,13 +3,22 @@ import React from 'react';
 const Menubar = () => (
   <div className="ui menu">
     <a className="header item" href="#top">
-      <img src="src/images/paw.png" className="ui mini image" alt="paw-ng" />
+      <i className="paw icon" />
       Dulce Palette
     </a>
     <div className="menu right">
-      <a className="item" href="#top">Signup</a>
-      <a className="item" href="#top">Login</a>
-      <a className="item" href="#top">Favorites</a>
+      <a className="item" href="#top">
+        <i className="user icon" />
+        Signup
+      </a>
+      <a className="item" href="#top">
+        <i className="user icon" />
+        Login
+      </a>
+      <a className="item" href="#top">
+        <i className="heart icon" />
+        Favorites
+      </a>
     </div>
   </div>
 );
