6bf43234034d87d2dc9ec589ba70ab076ab84f9e
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-07-24 17:34:06 -0700

    Updating Redux userActions and store

---
+++ src/Redux/userActions.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+const setUser = userObj => ({
+  type: 'LOGIN_USER',
+  payload: userObj,
+});
+
+const clearUser = () => ({
+  type: 'CLEAR_USER',
+});
+
+const registerUser = userObj => ({
+  type: 'NEW_USER',
+  payload: userObj,
+});
+
+const loginUser = userObj => dispatch => {
+  axios.post('http://localhost:3000/api/v1/auth/sign_in', userObj).then(res => {
+    dispatch(setUser(res.data.data));
+    localStorage.setItem('user', JSON.stringify(res.headers));
+  }).catch(error => {
+    throw (error);
+  });
+};
+
+const logoutUser = () => dispatch => {
+  const headers = JSON.parse(localStorage.user);
+  axios.delete('https://localhost:3000/api/v1/auth/sign_out', { headers });
+  dispatch(clearUser());
+  localStorage.clear();
+};
+
+const newUser = userObj => dispatch => {
+  axios.post('https://localhost:3000/api/v1/auth/', userObj).then(res => {
+    dispatch(registerUser(res.data.data));
+  }).catch(error => {
+    throw (error);
+  });
+};
+
+export default {
+  loginUser,
+  logoutUser,
+  newUser,
+};
