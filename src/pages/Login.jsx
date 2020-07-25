633740bd86a0d9720d901f7b517b56962c4daebd
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-07-24 17:33:32 -0700

    Updating login/signup components with redux

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,47 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button, Form, Grid, Header, Segment, Icon,
 } from 'semantic-ui-react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'proptypes';
+import userActions from '../Redux/userActions';
 import '../styles/styles-login.css';
 
-const LoginPage = () => (
-  <Grid textAlign="center" className="middle aligned wall login">
-    <Grid.Column className="login-column">
-      <Header as="h2" className="login-header" textAlign="center">
-        <Icon name="home" />
-        Log-in to your account
-      </Header>
-      <Form size="large">
-        <Segment stacked>
-          <Form.Input
-            id="form-input-control-email"
-            fluid
-            icon="user"
-            iconPosition="left"
-            type="text"
-            name="email"
-            placeholder="Email"
-            label="Email"
-            className="login-input"
-          />
-          <Form.Input
-            id="form-input-control-password"
-            fluid
-            icon="lock"
-            iconPosition="left"
-            type="password"
-            name="password"
-            placeholder="Password"
-            label="Password"
-            className="login-input"
-          />
-          <Button className="login-button" fluid size="large" type="submit">
-            Login
-          </Button>
-        </Segment>
-      </Form>
-    </Grid.Column>
-  </Grid>
-);
+const LoginPage = props => {
+  const dispatch = useDispatch();
+  const [loginForm, setLoginForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { history } = props;
+    dispatch(userActions.loginUser(loginForm));
+    history.push('/');
+  };
+
+  const handleChange = e => setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
+
+  const { email, password } = loginForm;
+
+  return (
+    <Grid textAlign="center" className="middle aligned wall login">
+      <Grid.Column className="login-column">
+        <Header as="h2" className="login-header" textAlign="center">
+          <Icon name="home" />
+          Log-in to your account
+        </Header>
+        <Form size="large" onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              id="form-input-control-email"
+              fluid
+              icon="user"
+              iconPosition="left"
+              type="text"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              placeholder="Email"
+              label="Email"
+              className="login-input"
+            />
+            <Form.Input
+              id="form-input-control-password"
+              fluid
+              icon="lock"
+              iconPosition="left"
+              type="password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+              placeholder="Password"
+              label="Password"
+              className="login-input"
+            />
+            <Button className="login-button" fluid size="large" type="submit">
+              Login
+            </Button>
+          </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+LoginPage.propTypes = {
+  history: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
 
 export default LoginPage;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,58 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'proptypes';
+import { useDispatch } from 'react-redux';
 import {
   Button, Form, Grid, Header, Segment, Icon,
 } from 'semantic-ui-react';
+import userActions from '../Redux/userActions';
 import '../styles/styles-signup.css';
 
-const SignupPage = () => (
-  <Grid textAlign="center" className="middle aligned wall signup">
-    <Grid.Column className="signup-column">
-      <Header as="h2" className="signup-header" textAlign="center">
-        <Icon name="user" />
-        Create your account
-      </Header>
-      <Form size="large">
-        <Segment stacked>
-          <Form.Input
-            id="form-input-control-email"
-            fluid
-            icon="user"
-            iconPosition="left"
-            type="text"
-            name="email"
-            placeholder="Email"
-            label="Email"
-            style={{ marginBottom: '1em' }}
-          />
-          <Form.Input
-            id="form-input-control-password"
-            fluid
-            icon="lock"
-            iconPosition="left"
-            type="password"
-            name="password"
-            placeholder="Password"
-            label="Password"
-            style={{ marginBottom: '1em' }}
-          />
-          <Form.Input
-            id="form-input-control-password-confirmation"
-            fluid
-            icon="lock"
-            iconPosition="left"
-            type="password"
-            name="password_confirmation"
-            placeholder="Password confirmation"
-            label="Password confirmation"
-            style={{ marginBottom: '1em' }}
-          />
-          <Button className="signup-button" fluid size="large" type="submit">
-            Signup
-          </Button>
-        </Segment>
-      </Form>
-    </Grid.Column>
-  </Grid>
-);
+const SignupPage = props => {
+  const dispatch = useDispatch();
+
+  const [signupForm, setSignupForm] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+  });
+
+  const handleChange = e => setSignupForm({ ...signupForm, [e.target.name]: e.target.value });
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { history } = props;
+    dispatch(userActions.newUser(signupForm));
+    history.push('/');
+  };
+
+  const {
+    email, password, passwordConfirmation,
+  } = signupForm;
+
+  return (
+    <Grid textAlign="center" className="middle aligned wall signup">
+      <Grid.Column className="signup-column">
+        <Header as="h2" className="signup-header" textAlign="center">
+          <Icon name="user" />
+          Create your account
+        </Header>
+        <Form size="large" onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              id="form-input-control-email"
+              fluid
+              icon="user"
+              iconPosition="left"
+              type="text"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              placeholder="Email"
+              label="Email"
+              style={{ marginBottom: '1em' }}
+            />
+            <Form.Input
+              id="form-input-control-password"
+              fluid
+              icon="lock"
+              iconPosition="left"
+              type="password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+              placeholder="Password"
+              label="Password"
+              style={{ marginBottom: '1em' }}
+            />
+            <Form.Input
+              id="form-input-control-password-confirmation"
+              fluid
+              icon="lock"
+              iconPosition="left"
+              type="password"
+              name="passwordConfirmation"
+              value={passwordConfirmation}
+              onChange={handleChange}
+              placeholder="Password confirmation"
+              label="Password confirmation"
+              style={{ marginBottom: '1em' }}
+            />
+            <Button className="signup-button" fluid size="large" type="submit">
+              Signup
+            </Button>
+          </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+SignupPage.propTypes = {
+  history: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
 
 export default SignupPage;
