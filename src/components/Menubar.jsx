91ff24c7207c510e0b65f18cfeafcb61a4cdfd13
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-07-24 17:32:46 -0700

    Updating Menubar component display if user is logged in, setting up logout link

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -1,28 +1,66 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { Menu } from 'semantic-ui-react';
+import userActions from '../Redux/userActions';
 
-const Menubar = () => (
-  <Menu>
-    <Link className="header item" to="/">
-      <i className="paw icon" />
-      Dulce Palette
-    </Link>
-    <Menu.Item className="menu right">
-      <Link className="item" href="/signup" to="/signup">
-        <i className="user icon" />
-        Signup
+const Menubar = () => {
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(userActions.logoutUser());
+  };
+  const user = useSelector(state => state.user);
+
+  const display = user.currentUser ? (
+    <Menu>
+      <Link className="header item" to="/">
+        <i className="paw icon" />
+        Dulce Palette
       </Link>
-      <Link className="item" href="/login" to="/login">
-        <i className="user icon" />
-        Login
+      <Menu.Item className="menu right">
+        <Link className="item" href="/signup" to="/signup">
+          <i className="user icon" />
+          Signup
+        </Link>
+        <Link className="item" href="/favorites" to="/favorites">
+          <i className="heart icon" />
+          Favorites
+        </Link>
+        <Link className="item" href="/" to="/" onClick={handleLogout}>
+          <i className="user icon" />
+          Logout
+        </Link>
+      </Menu.Item>
+    </Menu>
+  ) : (
+    <Menu>
+      <Link className="header item" to="/">
+        <i className="paw icon" />
+        Dulce Palette
       </Link>
-      <Link className="item" href="/favorites" to="/favorites">
-        <i className="heart icon" />
-        Favorites
-      </Link>
-    </Menu.Item>
-  </Menu>
-);
+      <Menu.Item className="menu right">
+        <Link className="item" href="/signup" to="/signup">
+          <i className="user icon" />
+          Signup
+        </Link>
+        <Link className="item" href="/login" to="/login">
+          <i className="user icon" />
+          Login
+        </Link>
+        <Link className="item" href="/favorites" to="/favorites">
+          <i className="heart icon" />
+          Favorites
+        </Link>
+      </Menu.Item>
+    </Menu>
+
+  );
+
+  return (
+    <nav className="ui container">
+      {display}
+    </nav>
+  );
+};
 
 export default Menubar;
