06d2dbe737dad199e6d5a77d34ee1c1d81919f95
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-07-27 12:29:25 -0700

    Linting fixes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 import ColorCard from './ColorCard';
 import Header from './Header';
 
-
 const Dashboard = () => {
   const colorArray = [];
   function hsvToRgb(hue, saturation, value) {
