70830623fd22e6c9524d5fd0142223e3fed103b0
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-07-27 08:17:58 -0700

    Adding Save Favorite button functionality to card

--- src/components/ColorCard.jsx
+++ src/components/ColorCard.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import PropTypes from 'proptypes';
 import { Button } from 'semantic-ui-react';
+import favoriteActions from '../Redux/favoritesActions';
 import ColorDrop from './ColorDrop';
 
-const ColorCard = ({ colorArray }) => (
-  <div className="ui raised card">
-    <div className="color-container">
-      {colorArray.map(color => (
-        <ColorDrop key={color} color={color} />
-      ))}
-      <Button content="Save" icon="heart" className="save-button" />
+const ColorCard = ({ colorArray }) => {
+  const currentUser = useSelector(state => state.user.currentUser, shallowEqual) || [];
+  const dispatch = useDispatch();
+  const addToFavorites = (colorArray, currentUser) => {
+    dispatch(favoriteActions.addToFavorites(colorArray, currentUser));
+  };
+
+  return (
+    <div className="ui raised card">
+      <div className="color-container">
+        {colorArray.map(color => (
+          <ColorDrop key={color} color={color} />
+        ))}
+        <Button content="Save" icon="heart" className="save-button" onClick={() => addToFavorites(colorArray, currentUser)} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 ColorCard.propTypes = {
   colorArray: PropTypes.instanceOf(Array).isRequired,
