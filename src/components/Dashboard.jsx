7ee4a1a28eb92700ad0080056e5ef8cca3cd5908
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-08-31 18:29:52 -0700

    Clearing up Dashboard component, setting up colorCard component to dispatch redux actions for favorites

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,25 +1,13 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-mixed-operators */
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+import React from 'react';
 import ColorCard from './ColorCard';
 import Header from './Header';
-import paletteActions from '../Redux/paletteActions';
 
-const Dashboard = () => {
-  const colorArray = useSelector(state => state.palette.palettes, shallowEqual) || [];
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(paletteActions.loadPalette());
-  }, [dispatch]);
-
-  return (
-    <div className="ui container">
-      <Header />
-      <ColorCard colorArray={colorArray} />
-    </div>
-  );
-};
-
+const Dashboard = () => (
+  <div className="ui container">
+    <Header />
+    <ColorCard />
+  </div>
+);
 export default Dashboard;
